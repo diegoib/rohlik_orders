2dcefbbb5ae7d3ae7bac5f225f5148fb4cb631d4
Diego Iglesias Bayo <38340341+diegoib@users.noreply.github.com>
2024-08-30 11:36:56 +0000

    advenced on config core

--- model-package/model/config/core.py
+++ model-package/model/config/core.py
@@ -13,3 +13,20 @@ CONFIG_FILE_PATH = PACKAGE_ROOT / "config.yml"
 DATASET_DIR = PACKAGE_ROOT / "datasets"
 TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
 
+class AppConfig(BaseModel):
+    """
+    Application-level config
+    """
+
+    training_data_file: str
+    test_data_file: str
+    pipeline_save_file: str
+
+
+class ModelConfing(BaseModel):
+    """
+    All configuration relevant to model
+    trainig and feature engineering.
+    """
+    d
+
